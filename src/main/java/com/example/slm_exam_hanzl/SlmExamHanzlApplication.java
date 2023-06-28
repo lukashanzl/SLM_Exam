00930811046e04628aa9e5f7ea933311baff5902
lukashanzl <wi20b103@technikum-wien.at>
2023-06-28 16:13:17 +0200

    initial commit

---
+++ src/main/java/com/example/slm_exam_hanzl/SlmExamHanzlApplication.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam_hanzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmExamHanzlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmExamHanzlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slm_exam_hanzl/SlmExamHanzlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slm_exam_hanzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmExamHanzlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
